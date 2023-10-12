ade72d362448340c50dab8ea565b97b5814309c5
skrtjd <147685812+skrtjd@users.noreply.github.com>
2023-10-12 14:10:34 +0900

    Add files via upload

--- cbnufinal23.ino
+++ cbnufinal23.ino
@@ -1,11 +1,5 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
-#include <DHT.h>
-
-#define DHTPIN 2
-#define DHTTYPE DHT11
-
-DHT dht(DHTPIN, DHTTYPE);
 
 // Set the LCD address to 0x27 for a 16 chars and 2 line display
 LiquidCrystal_I2C lcd(0x27, 16, 2);
@@ -13,7 +7,6 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 void setup()
 {
   Serial.begin(9600);
-  dht.begin();
 	// initialize the LCD
 	lcd.begin();
 
@@ -23,22 +16,12 @@ void setup()
 
 void loop()
 {
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-
-  if(isnan(h) || isnan(t)){
-    Serial.println("Failed to read from DHT sensor!");
-    return;
-  }
-
   lcd.setCursor(0,0); //첫번째 줄의 0번째 부터 출력
   lcd.print("Humidity: ");
-  lcd.print(h);
   lcd.print("%");
 
   lcd.setCursor(0,1); //두번째 줄의 0번째 부터 력출
   lcd.print("Temp: ");
-  lcd.print(t);
   lcd.println(" *C");
 
   delay(2000);
